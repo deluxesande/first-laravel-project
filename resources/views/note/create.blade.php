f7db45ea950c412db139175c4f8595c9eb8ae49b
deluxesande <deluxesande@gmail.com>
2024-05-18 20:21:39 +0300

    (Add): note view rendering

---
+++ resources/views/note/create.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+    <h1>create</h1>
+</div>
---
+++ resources/views/note/edit.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+    <h1>Edit</h1>
+</div>
